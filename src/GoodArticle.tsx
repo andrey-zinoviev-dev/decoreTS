c197ac5e547fd0433674aecbdb9ae3750a05c7dc
Andrey <sttrog_810@mail.ru>
2024-03-08 11:36:58 -0800

    reconfiguring goods layout

--- src/GoodArticle.tsx
+++ src/GoodArticle.tsx
@@ -1,7 +1,9 @@
 import { goodModel } from "./models/goodModel"
 import { useNavigate } from "react-router-dom"
 import "./GoodArticle.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { icon } from "@fortawesome/fontawesome-svg-core";
 interface goodProps {
     good: goodModel,
 }
@@ -16,10 +18,24 @@ export default function GoodArticle({good}: goodProps) {
             </div> */}
             <img className="main__goods-ul-li-article-img" src={good.cover}></img>
             <div className="main__goods-ul-li-article-text">
-                <h3>{good.name}</h3>
-                <p>В наличии: {good.stock}</p>
+                <div className="main__goods-ul-li-article-text-main">
+                    <span>{good.name}</span>
+                    <div className="main__goods-ul-li-article-text-main-author">
+                        <span className="main__goods-ul-li-article-text-main-author-name">{good.designer.name}</span>
+                        <img className="main__goods-ul-li-article-text-main-author-pic" src={good.designer.avatar} alt="" />
+                    </div>
+                    {/* <span className="main__goods-ul-li-article-text-main-author">{good.designer.name}</span> */}
+                </div>
+                {/* <div className="main__goods-ul-li-article-text-designer">
+                    <img src={good.designer.avatar}></img>
+                    <div className="main__goods-ul-li-article-text-designer-div">
+                        <h3>{good.designer.name}</h3>
+                        <FontAwesomeIcon className="svg-arrow" icon={faArrowRight} />
+                    </div>
+                </div> */}
+                <p>В наличии: {good.stock} <div></div></p>
                 {/* <span>Автор - {good.designer.name}</span> */}
-                <button onClick={() => {
+                <button className="main__goods-ul-li-article-btn" onClick={() => {
                     navigate({
                         pathname: `goods/${good.name}`
                     }, {
